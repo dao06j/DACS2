788a3cc53d0906e5d3855429cb4ce9eb235738af
dao06j <daotn.24it@vku.udn.vn>
2025-12-10 07:40:56 +0700

    Update 2025_12_09_213014_sanpham.php
    Sua migrate

--- database/migrations/2025_12_09_213014_sanpham.php
+++ database/migrations/2025_12_09_213014_sanpham.php
@@ -10,25 +10,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('sanpham', function (Blueprint $table) {
-            
-            $table->string('MaSP', 20)->primary(); 
+    $table->string('MaSP', 10)->primary();
 
-            $table->string('TenSP', 255);
-            $table->text('MoTa');
+    $table->string('TenSP', 255);
+    $table->text('MoTa');
 
-            $table->string('LoaiSP', 100)->index(); 
+    $table->string('LoaiSP', 100)->index();
 
-            $table->decimal('DonGia', 10, 2); 
-            
-            $table->string('HinhAnh', 255)->nullable(); 
-            
-            $table->integer('SLTonKho')->default(0);
-            $table->integer('SLDaBan')->default(0);
+    $table->decimal('DonGia', 10, 2);
 
-            $table->string('TrangThai', 50)->default('Hiện');
+    $table->string('HinhAnh', 255)->nullable();
 
-            $table->timestamps();
-        });
+    $table->integer('SLTonKho')->default(0);
+    $table->integer('SLDaBan')->default(0);
+
+    $table->string('TrangThai', 50)->default('Hiện');
+
+    $table->timestamps();
+});
     }
 
     public function down(): void
