8082e314817b381ceb4347996f8b9d099f245dc6
dao06j <daotn.24it@vku.udn.vn>
2025-12-10 07:40:23 +0700

    Update 2025_12_09_053210_danhgia.php
    sua migrate

--- database/migrations/2025_12_09_053210_danhgia.php
+++ database/migrations/2025_12_09_053210_danhgia.php
@@ -9,22 +9,22 @@ return new class extends Migration
     public function up()
     {
         Schema::create('danh_gia', function (Blueprint $table) {
-            $table->string('MaDG', 10)->primary(); 
-            $table->string('MaKH', 10);
-            $table->string('MaSP', 10);
-            $table->string('NoiDung', 1000);
-            $table->date('NgayDG');
-            $table->timestamps();
+    $table->string('MaDG', 10)->primary();
+    $table->string('MaKH', 10);
+    $table->string('MaSP', 10);
 
-            // Foreign keys
-            $table->foreign('MaKH')->references('MaKH')->on('khach_hang')->onDelete('cascade');
-            $table->foreign('MaSP')->references('MaSP')->on('sanpham')->onDelete('cascade');
-            
-            // Index
-            $table->index('MaSP');
-            $table->index('MaKH');
-            $table->index('NgayDG');
-        });
+    $table->string('NoiDung', 1000);
+    $table->date('NgayDG');
+
+    $table->timestamps();
+
+    $table->foreign('MaKH')->references('MaKH')->on('khach_hang')->onDelete('cascade');
+    $table->foreign('MaSP')->references('MaSP')->on('sanpham')->onDelete('cascade');
+
+    $table->index('MaSP');
+    $table->index('MaKH');
+    $table->index('NgayDG');
+});
     }
 
     public function down()
